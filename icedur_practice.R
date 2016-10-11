a048c6fecb32a94b6840337058b3eef6c3d9ad95
vbutitta <vbutitta@wisc.edu>
2016-10-11 11:31:33 -0500

    Creation of practice script for others to edit

---
+++ icedur_practice.R
@@ -0,0 +1,28 @@
+####################################################
+# This is a test script for all Stanley lab members to practice pulling and pushing between R studio and GitHub
+
+# Directions: Each person should add one of the suggested lines of code to this script, next week we will look at how all these different edits from different people are handled by git
+
+####################################################
+
+ice.dur <- read.csv("C:/Users/Vincent/Downloads/north_temperate_lakes_lter__ice_duration_-_madison_lakes_area.csv") # VLB
+save.image("~/stanleylab-practicerepo/ice.dur.RData") # VLB
+
+plot(ice.dur$year4, ice.dur$ice_duration)
+
+
+# someone add a trendline
+# someone calculate a model fit
+# someone make the plot prettier
+# someone write an autumn-themed haiku
+# someone give an example of how to plot an example of these data in ggplot
+# someone work on a different project in R (freebie) (comment it in here so we don't all take this one!)
+# someone subset these data to omit all NAs for ice_duration
+# someone make edits on the newer plotting
+# someone delete this comment
+
+
+
+
+
+# fix or delete one of the lines of code below
